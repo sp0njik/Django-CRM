ac4bb633f893f64371cc4048f21a2c1d3095a8cf
Artak Sarkisyan <artak.sarkisyan92@gmail.com>
2024-04-16 14:53:53 +0300

    Crate A Record

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from website.models import Record
 
-# Register your models here.
+admin.site.register(Record)
+
--- website/views.py
+++ website/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+
 from website.forms import SignUpForm
+from website.models import Record
 
 
 def home(request):
+    records = Record.objects.all()
+
     # Check to see if logging in
     if request.method == 'POST':
         username = request.POST['username']
@@ -19,7 +23,7 @@ def home(request):
             messages.success(request, 'There Was An Error Logging In, Please Try Again ...')
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records': records})
 
 
 def logout_user(request):
@@ -44,3 +48,13 @@ def register_user(request):
         form = SignUpForm()
         return render(request, 'register.html', {'form': form})
     return render(request, 'register.html', {'form': form})
+
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        # Look Up Records
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record': customer_record})
+    else:
+        messages.success(request, 'You Must Logged In To View Page...')
+        return redirect('home')
